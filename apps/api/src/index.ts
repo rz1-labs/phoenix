68b65797cf56c1c59264fa0bf29c6c74910be3d1
mdrizwanfk <rizwanfardeenkhan@outlook.com>
2026-03-08 07:06:20 +0530

    build(api): iterate over this

---
+++ apps/api/src/index.ts
@@ -0,0 +1,9 @@
+import { createServer } from "./server";
+import { log } from "@phoenix/logger";
+
+const port = process.env.PORT || 3002;
+const server = createServer();
+
+server.listen(port, () => {
+  log(`api running on ${port}`);
+});
